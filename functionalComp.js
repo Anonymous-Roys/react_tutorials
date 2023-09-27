056d091b3305263ad84776f01b62dbf8bff359fd
David Arhin <davidarhin2005@gmail.com>
2023-09-27 00:53:53 +0000

    Functional Components

--- functionalComp.js
+++ functionalComp.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom/client';
 
 
 function FunctionalComp() {
-    return <p>This is a Functional Component</p>
+    const color = "red";
+    const carInfo = {
+        color: "yellow",
+        type: "Ford"
+    };;
+    return (
+        <div align = "center">
+            <p>This is a Functional Component</p>
+            <FunctionalComp1 brand="Mustang" color={color} name={ carInfo } />
+        </div>
+    )
 }
 
-
+function FunctionalComp1(props) {
+    
+    return (
+        <p>It is a { props.color } { props.brand } { props.name.type }</p>
+    )
+}
 
 export default FunctionalComp;
 
