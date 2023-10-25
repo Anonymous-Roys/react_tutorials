6b6d461f52ae5f2c4a50a4ad468d669a88dce408
David Arhin <davidarhin2005@gmail.com>
2023-10-25 09:23:48 +0000

    navbar

--- realsite/Navbar.js
+++ realsite/Navbar.js
@@ -1,14 +1,14 @@
 // Navbar.js
 import React from "react";
-import style from "./Compstyle.module.css"; // Import the CSS file
+// import "./Navbar.css"; // Import the CSS file
 
 function Navbar() {
     return (
-        <div className={style.navbar}>
-            <div className={style.logo}>Your Logo</div>
-            <ul className={style.li}>
-                <li className={style.li}>
-                    <a href="./"  className={style.a}>Home</a>
+        <div>
+            <div>Your Logo</div>
+            <ul>
+                <li>
+                    <a href="./">Home</a>
                 </li>
                 <li>
                     <a href="./about">About</a>
